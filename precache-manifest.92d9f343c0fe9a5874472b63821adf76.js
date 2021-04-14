5728ce7fa045b124d67012cda65f37d061c89af7
Moshe Pinhasi <pi.moshe@gmail.com>
2021-04-14 10:40:48 +0300

    deploy

---
+++ precache-manifest.92d9f343c0fe9a5874472b63821adf76.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4520e3d8b68ce7a6013",
+    "url": "/css-height-example/css/app.f01f90b7.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/css-height-example/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "051d805e8b60d6a895105202339fa1b6",
+    "url": "/css-height-example/index.html"
+  },
+  {
+    "revision": "94528282205ed2adc3ad",
+    "url": "/css-height-example/js/about.86ab6dc7.js"
+  },
+  {
+    "revision": "f4520e3d8b68ce7a6013",
+    "url": "/css-height-example/js/app.c709bd2f.js"
+  },
+  {
+    "revision": "180f40350c17e363cb00",
+    "url": "/css-height-example/js/chunk-vendors.2b3c3abe.js"
+  },
+  {
+    "revision": "edb516158b3e51b04a4fc372021888bf",
+    "url": "/css-height-example/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/css-height-example/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/css-height-example/precache-manifest.92d9f343c0fe9a5874472b63821adf76.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "css-height-example"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
