85617dd8dfe14513d88b3a5747e811b3b16f371d
Moshe Pinhasi <pi.moshe@gmail.com>
2021-04-14 10:28:52 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="header center">header</div>
+    <div class="body">body</div>
+    <div class="footer center">
+      <button>btn example</button>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.header {
+  height: 40px;
+  background-color: burlywood;
+  color: white;
 }
+
+.body {
+  flex: 1;
+}
+
+.footer {
+  height: 40px;
+  background-color:cadetblue;
+  color: white;
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 </style>
